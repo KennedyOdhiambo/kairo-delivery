ec7ee8d7370ac5b17e299c54309e2a0187938744
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2025-03-14 22:20:43 +0300

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,48 @@
+import { Tabs } from 'expo-router';
+import { Home, ShoppingCart, User } from 'lucide-react-native';
+
+import '../global.css';
+
+export default function AppLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#3b82f6',
+        tabBarInactiveTintColor: '#6b7280',
+        tabBarStyle: {
+          paddingBottom: 5,
+          paddingTop: 5,
+          height: 60,
+        },
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Home size={24} color={color} strokeWidth={focused ? 2.5 : 1.5} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Cart',
+          tabBarIcon: ({ color, focused }) => (
+            <ShoppingCart size={24} color={color} strokeWidth={focused ? 2.5 : 1.5} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <User size={24} color={color} strokeWidth={focused ? 2.5 : 1.5} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
