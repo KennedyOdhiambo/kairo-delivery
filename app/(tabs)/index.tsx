4af37854b6995ab4e58dd7a83fc58b9fb0e09821
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2025-03-23 18:27:51 +0300

    Implement animated navigation bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,12 +6,137 @@ import { Animated, Pressable, SectionList, StatusBar, Text, View } from 'react-n
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { products } from 'data/products'
 
+function NavBar({ scrollY }: { scrollY: Animated.Value }) {
+  const insets = useSafeAreaInsets()
+
+  const headerTranslateY = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [-124, 0],
+    extrapolate: 'clamp',
+  })
+  return (
+    <Animated.View
+      className="absolute top-0 left-0 right-0 z-50 bg-white px-4 py-3 border-b border-gray-200 flex-row justify-between items-center"
+      style={{
+        paddingTop: insets.top + 16,
+        transform: [{ translateY: headerTranslateY }],
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        elevation: 1,
+      }}>
+      <View className="p-2.5 z-20 bg-gray-100 rounded-full">
+        <Ionicons name="arrow-back" size={20} color="black" />
+      </View>
+      <Text className="text-lg font-medium">Okoth's Joint</Text>
+      <View className="flex-row gap-3">
+        <View className="p-2.5 z-10 bg-gray-100 rounded-full">
+          <Ionicons name="search" size={20} color="black" />
+        </View>
+      </View>
+    </Animated.View>
+  )
+}
+
+function ListItem({
+  item,
+  cartItems,
+  setCartItems,
+}: {
+  item: (typeof products)[number]['items'][number]
+  cartItems: Record<string, { quantity: number; price: number }>
+  setCartItems: React.Dispatch<
+    React.SetStateAction<Record<string, { quantity: number; price: number }>>
+  >
+}) {
+  const itemId = item.name
+  const cartItem = cartItems[itemId]
+  const quantity = cartItem?.quantity || 0
+  return (
+    <View className="flex-row py-4 relative bg-white px-6 border-b border-gray-200">
+      <Image
+        source={item.image}
+        style={{ width: 108, height: 108, borderRadius: 12 }}
+        contentFit="cover"
+      />
+      <View className="flex-1 ml-4 flex-row gap-4">
+        <View className="flex-col flex-1">
+          <Text className="text-lg">{item.name}</Text>
+          <Text className="text-gray-500" numberOfLines={4} ellipsizeMode="tail">
+            {item.description}
+          </Text>
+        </View>
+
+        <View className="flex-col items-end justify-between">
+          <Text className="text-lg text-gray-700">KSh {item.price}</Text>
+          <View className="flex-row items-center w-24 justify-end">
+            {quantity > 0 && (
+              <Pressable
+                onPress={() => {
+                  setCartItems((prev) => {
+                    const newQuantity = cartItem.quantity - 1
+                    if (newQuantity === 0) {
+                      const newItems = { ...prev }
+                      delete newItems[itemId]
+                      return newItems
+                    } else {
+                      return {
+                        ...prev,
+                        [itemId]: {
+                          ...cartItem,
+                          quantity: newQuantity,
+                        },
+                      }
+                    }
+                  })
+                }}
+                className="rounded-full bg-gray-100 p-1 items-center justify-center">
+                <Ionicons name="remove" size={18} color="#374151" />
+              </Pressable>
+            )}
+
+            {quantity > 0 && (
+              <Text className="mx-2 font-medium text-base min-w-6 text-center">{quantity}</Text>
+            )}
+
+            <Pressable
+              onPress={() => {
+                setCartItems((prev) => {
+                  if (cartItem) {
+                    return {
+                      ...prev,
+                      [itemId]: {
+                        ...cartItem,
+                        quantity: cartItem.quantity + 1,
+                      },
+                    }
+                  } else {
+                    return {
+                      ...prev,
+                      [itemId]: {
+                        quantity: 1,
+                        price: item.price || 0,
+                      },
+                    }
+                  }
+                })
+              }}
+              className="rounded-full bg-green-200 p-1 items-center justify-center">
+              <Ionicons name="add" size={18} color="#1a7c3a" />
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
 export default function Home() {
   const [cartItems, setCartItems] = useState<Record<string, { quantity: number; price: number }>>(
     {}
   )
-  const insets = useSafeAreaInsets()
-
+  const scrollY = useRef(new Animated.Value(0)).current
   const totalCost = Object.values(cartItems).reduce(
     (sum, item) => sum + item.price * item.quantity,
     0
@@ -34,6 +159,7 @@ export default function Home() {
   return (
     <View className="flex-1 bg-background relative">
       <StatusBar translucent backgroundColor="transparent" barStyle="default" />
+      <NavBar scrollY={scrollY} />
 
       <View className="absolute top-0 left-0 right-0 h-52">
         <Image
@@ -53,6 +179,10 @@ export default function Home() {
         keyExtractor={(item, index) => item.name + index}
         stickySectionHeadersEnabled={false}
         contentContainerStyle={{ paddingBottom: 40 }}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+          useNativeDriver: false,
+        })}
+        scrollEventThrottle={16}
         ListHeaderComponent={() => (
           <>
             <View className="pt-12 px-6 flex-row justify-between">
@@ -69,7 +199,7 @@ export default function Home() {
               </View>
             </View>
 
-            <View className="bg-background rounded-tr-[64px] px-6 mt-6 pt-10">
+            <View className="bg-background rounded-tr-[64px] px-6 mt-12 pt-10">
               <Text className="text-3xl text-gray-900 font-semibold tracking-tight">
                 Okoth's Joint
               </Text>
@@ -81,91 +211,9 @@ export default function Home() {
             <Text className="font-semibold text-gray-900 text-xl">{title}</Text>
           </View>
         )}
-        renderItem={({ item }) => {
-          const itemId = item.name
-          const cartItem = cartItems[itemId]
-          const quantity = cartItem?.quantity || 0
-
-          return (
-            <View className="flex-row py-4 relative bg-white px-6 border-b border-gray-200">
-              <Image
-                source={item.image}
-                style={{ width: 120, height: 120, borderRadius: 12 }}
-                contentFit="cover"
-              />
-              <View className="flex-1 ml-4 flex-row gap-4">
-                <View className="flex-col flex-1">
-                  <Text className="text-lg">{item.name}</Text>
-                  <Text className="text-gray-500" numberOfLines={4} ellipsizeMode="tail">
-                    {item.description}
-                  </Text>
-                </View>
-
-                <View className="flex-col items-end justify-between">
-                  <Text className="text-lg text-gray-700">KSh {item.price}</Text>
-                  <View className="flex-row items-center w-24 justify-end">
-                    {quantity > 0 && (
-                      <Pressable
-                        onPress={() => {
-                          setCartItems((prev) => {
-                            const newQuantity = cartItem.quantity - 1
-                            if (newQuantity === 0) {
-                              const newItems = { ...prev }
-                              delete newItems[itemId]
-                              return newItems
-                            } else {
-                              return {
-                                ...prev,
-                                [itemId]: {
-                                  ...cartItem,
-                                  quantity: newQuantity,
-                                },
-                              }
-                            }
-                          })
-                        }}
-                        className="rounded-full bg-gray-100 p-1 items-center justify-center">
-                        <Ionicons name="remove" size={20} color="#374151" />
-                      </Pressable>
-                    )}
-
-                    {quantity > 0 && (
-                      <Text className="mx-2 font-medium text-base min-w-6 text-center">
-                        {quantity}
-                      </Text>
-                    )}
-
-                    <Pressable
-                      onPress={() => {
-                        setCartItems((prev) => {
-                          if (cartItem) {
-                            return {
-                              ...prev,
-                              [itemId]: {
-                                ...cartItem,
-                                quantity: cartItem.quantity + 1,
-                              },
-                            }
-                          } else {
-                            return {
-                              ...prev,
-                              [itemId]: {
-                                quantity: 1,
-                                price: item.price || 0,
-                              },
-                            }
-                          }
-                        })
-                      }}
-                      className="rounded-full bg-green-200 p-1 items-center justify-center">
-                      <Ionicons name="add" size={20} color="#1a7c3a" />
-                    </Pressable>
-                  </View>
-                </View>
-              </View>
-            </View>
-          )
-        }}
+        renderItem={({ item }) => (
+          <ListItem item={item} cartItems={cartItems} setCartItems={setCartItems} />
+        )}
       />
 
       <Animated.View
